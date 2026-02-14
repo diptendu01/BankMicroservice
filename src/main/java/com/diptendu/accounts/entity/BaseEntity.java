ed2d5358f4dbe9b5e1ce88d143e176b6de06b6a6
diptendu01 <ddas2367@gmail.com>
2026-02-14 16:03:43 +0530

    V_2.0_14th_F

---
+++ src/main/java/com/diptendu/accounts/entity/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.diptendu.accounts.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+@MappedSuperclass
+@Getter
+@Setter
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+
+public class BaseEntity {
+    @CreatedDate
+    @Column(name = "created_at", nullable = false,updatable = false)
+    private LocalDateTime createdAt;
+    @CreatedBy
+    @Column(name = "created_by",updatable = false)
+    private String createdBy;
+    @Column(insertable = false)
+    private LocalDateTime updatedAt;
+    @Column(insertable = false)
+    private String updatedBy;
+
+}
